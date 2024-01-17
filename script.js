a90fcb127e3bb57b2e4f17c80ec77bf6209391ab
Patrik Pallagi <113801545+pallagip@users.noreply.github.com>
2024-01-17 23:46:18 +0100

    NEW JS FOR SCROLLING 1

--- script.js
+++ script.js
@@ -1,24 +1,19 @@
 document.addEventListener('DOMContentLoaded', function() {
     var iframe = document.querySelector('.youtube-container iframe');
+    var isScrolling;
 
-    // This function is called when the iframe is clicked
-    function onIframeClick() {
-        // We check if pointer-events are already enabled
-        // If they are not, we enable them so the video can be interacted with
-        // If they are, we disable them to allow scrolling
-        if (iframe.style.pointerEvents === 'none') {
-            iframe.style.pointerEvents = 'auto';
-        } else {
-            iframe.style.pointerEvents = 'none';
-        }
-    }
-
-    // We add an event listener to the iframe
-    // When the iframe is clicked, we call the onIframeClick function
-    iframe.addEventListener('click', onIframeClick);
-
-    // When the user scrolls, we disable pointer-events to ensure scrolling is possible
+    // Disable pointer events while scrolling
     window.addEventListener('scroll', function() {
+        clearTimeout(isScrolling);
         iframe.style.pointerEvents = 'none';
+
+        isScrolling = setTimeout(function() {
+            iframe.style.pointerEvents = 'auto';
+        }, 66); // Delay of 66ms corresponds to ~15fps
+    }, false);
+
+    // Enable pointer events after clicking on the video
+    iframe.addEventListener('click', function() {
+        iframe.style.pointerEvents = 'auto';
     });
 });
