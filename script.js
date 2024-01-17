aca106591e8592f8d81ffec1b5873d98b1309976
Patrik Pallagi <113801545+pallagip@users.noreply.github.com>
2024-01-17 23:50:33 +0100

    Tap enabled

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
 document.addEventListener('DOMContentLoaded', function() {
     var iframe = document.querySelector('.youtube-container iframe');
-    var isScrolling;
+    var startY;
 
-    // Disable pointer events while scrolling
-    window.addEventListener('scroll', function() {
-        clearTimeout(isScrolling);
-        iframe.style.pointerEvents = 'none';
+    // Detect start of touch
+    iframe.addEventListener('touchstart', function(event) {
+        startY = event.touches[0].pageY;
+    }, {passive: true});
 
-        isScrolling = setTimeout(function() {
+    // Detect end of touch
+    iframe.addEventListener('touchend', function(event) {
+        var endY = event.changedTouches[0].pageY;
+
+        // Check if the touch was more of a tap than a swipe
+        if (Math.abs(startY - endY) < 10) {
+            // It was a tap, enable pointer-events temporarily
             iframe.style.pointerEvents = 'auto';
-        }, 66); // Delay of 66ms corresponds to ~15fps
-    }, false);
 
-    // Enable pointer events after clicking on the video
-    iframe.addEventListener('click', function() {
-        iframe.style.pointerEvents = 'auto';
-    });
+            // Use a timeout to reset pointer-events to none after a short period
+            setTimeout(function() {
+                iframe.style.pointerEvents = 'none';
+            }, 500); // Adjust the timeout to your preference
+        }
+    }, {passive: true});
 });
+
